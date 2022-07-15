b4d65a90965c08939d284f72ba2c6681c3ac9619
pipe-felipe <alves.felipepipe@gmail.com>
2022-07-15 19:58:23 -0300

    add prettier

--- src/components/Customer.tsx
+++ src/components/Customer.tsx
@@ -1,4 +1,4 @@
-import {useFetch} from "../hooks/useFetch";
+import { useFetch } from "../hooks/useFetch"
 import Url from "../apiUrls.json"
 
 type ProfilePicture = {
@@ -35,63 +35,59 @@ type User = {
 }
 
 export default function Customer() {
-
-    const {data: dogProfile} = useFetch<ProfilePicture>(Url.dogProfile)
-    const {data: userData} = useFetch<User[]>(Url.user)
+    const { data: dogProfile } = useFetch<ProfilePicture>(Url.dogProfile)
+    const { data: userData } = useFetch<User[]>(Url.user)
 
     return (
-        <div className="bg-amber-800 w-72 h-72">
-
-            {userData?.map(user => {
+        <div className="h-72 w-72 bg-amber-800">
+            {userData?.map((user) => {
                 return (
                     <div>
-
-                        <div className="flex justify-center items-center">
-                            <div className="z-50 translate-y-1.5 bg-amber-50 w-20 h-20 rounded-3xl flex">
-                                <img className="object-fill border-2 w-20 h-20 rounded-3xl" src={dogProfile?.message} alt="dog"/>
+                        <div className="flex items-center justify-center">
+                            <div className="z-50 flex h-20 w-20 translate-y-1.5 rounded-3xl bg-amber-50">
+                                <img
+                                    className="h-20 w-20 rounded-3xl border-2 object-fill"
+                                    src={dogProfile?.message}
+                                    alt="dog"
+                                />
                             </div>
                         </div>
 
-                        <div
-                            className="-z-10 m-auto -translate-y-8 border-2 rounded-2xl w-11/12 h-4/5 grid grid-cols-1 place-content-center">
-
-                            <div className="text-center mt-5">
+                        <div className="-z-10 m-auto grid h-4/5 w-11/12 -translate-y-8 grid-cols-1 place-content-center rounded-2xl border-2">
+                            <div className="mt-5 text-center">
                                 <input
                                     type="text"
                                     readOnly={true}
-                                    className="border w-11/12 ml-1 mr-1 rounded-md p-1"
+                                    className="ml-1 mr-1 w-11/12 rounded-md border p-1"
                                     name="document"
                                     value={user.document}
                                 />
                                 <input
                                     type="text"
                                     readOnly={true}
-                                    className="mt-1 border w-11/12 ml-1 mr-1 rounded-md p-1"
+                                    className="mt-1 ml-1 mr-1 w-11/12 rounded-md border p-1"
                                     name="document"
                                     value="55448487569"
                                 />
                                 <input
                                     type="text"
                                     readOnly={true}
-                                    className="mt-1 border w-11/12 ml-1 mr-1 rounded-md p-1"
+                                    className="mt-1 ml-1 mr-1 w-11/12 rounded-md border p-1"
                                     name="document"
                                     value="55448487569"
                                 />
                                 <input
                                     type="text"
                                     readOnly={true}
-                                    className="mt-1 border w-11/12 ml-1 mr-1 rounded-md p-1"
+                                    className="mt-1 ml-1 mr-1 w-11/12 rounded-md border p-1"
                                     name="document"
                                     value="55448487569"
                                 />
                             </div>
-
                         </div>
-
                     </div>
                 )
             })}
-
         </div>
     )
 }
