93b62c27281e7f5f4b94d70af73471d7c70c7226
pipe-felipe <alves.felipepipe@gmail.com>
2022-07-19 15:22:40 -0300

    wip

--- src/components/Customers.tsx
+++ src/components/Customers.tsx
@@ -2,6 +2,7 @@ import Customer from "./Customer"
 import { CustomerType } from "../types/CustomerType"
 import { useFetch } from "../hooks/useFetch"
 import URL from "../apiUrls.json"
+import ReactPaginate from "react-paginate"
 
 export default function Customers() {
   const { data } = useFetch<CustomerType[]>(URL.tccRandom)
@@ -9,8 +10,21 @@ export default function Customers() {
 
   if (data === null) return <h1>There is no data</h1>
 
+  const handlerPageClick = (data: any) => {
+    console.log(data.selected)
+  }
+
   return (
     <>
+      <ReactPaginate
+        previousLabel={"Before"}
+        nextLabel={"Next"}
+        breakLabel={"..."}
+        pageCount={25}
+        marginPagesDisplayed={3}
+        onPageChange={handlerPageClick}
+      />
+
       {data.map((customer) => {
         return <Customer key={customer.id} {...customer} />
       })}
